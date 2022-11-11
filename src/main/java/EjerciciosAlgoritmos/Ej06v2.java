f937a1bbd22ffe2e7c3ca6a1c34c0adcda189df7
Daniel Navarro <dnavmoy119@g.educaand.es>
2022-11-11 08:54:32 +0100

    empiezo ejercicio 6 en ej06v2

---
+++ src/main/java/EjerciciosAlgoritmos/Ej06v2.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package EjerciciosAlgoritmos;
+
+/**
+ *
+ * @author danielnavarro
+ */
+public class Ej06v2 {
+    public static void main(String[] args) {
+        int numeroPrimero,numeroSegundo,numeroTercero;
+        
+        numeroPrimero=Ej01.pedirNumero("cual es la cantidad la primera persona");
+        numeroSegundo=Ej01.pedirNumero("cual es la cantidad del segundo?");
+        numeroTercero=Ej01.pedirNumero("Cual es la tercera cantidad?");
+        
+        
+    }
+}
