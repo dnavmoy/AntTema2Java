decc908842e02af447d392cf24a7cec6f5c6397f
dnavmoy119 <dnavmoy119@g.educaand.es>
2022-11-23 09:11:51 +0100

    probando ejercicio 7

---
+++ src/main/java/EjerciciosAlgoritmos/Ej07.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package EjerciciosAlgoritmos;
+
+/**
+ *
+ * @author daniel
+ */
+public class Ej07 {
+    public static void main(String[] args) {
+        double tipoConversion=Ej01.pedirNumero("cual es el tipo de conversion?");
+        double cantidadEuros=Ej01.pedirNumero("Cuantos euros");
+        double cantidadDolares=cantidadEuros*tipoConversion;
+        
+        System.out.println("El total de dolares es: %d".formatted(cantidadDolares));
+       
+            
+    }
+}
