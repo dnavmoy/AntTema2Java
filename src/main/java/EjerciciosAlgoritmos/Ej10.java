fe6b28a08b0840f09f76de36f47bd29f11991298
dnavmoy <dnavmoy119@g.educaand.es>
2022-12-09 18:56:10 +0000

    tratando de mejorar el joption para que solo puedas meter numeros

--- src/main/java/EjerciciosAlgoritmos/Ej10.java
+++ src/main/java/EjerciciosAlgoritmos/Ej10.java
@@ -1,5 +1,7 @@
 package EjerciciosAlgoritmos;
 
+import javax.swing.JOptionPane;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -12,13 +14,20 @@ package EjerciciosAlgoritmos;
 public class Ej10 {
 
     public static void main(String[] args) {
-        double ladoA;
+        double ladoA, ladoB, ladoC;
         
-        ladoA=Ej07.pedirNumeroDouble("Introduce lado A");
-        //ladoB=Ej07.pedirNumeroDouble("introduce lado B");
-        //ladoC=Ej07.pedirNumeroDouble("Introduce lado C");
+        ladoA=pedirNumeroJO("Introduce lado A");
+        ladoB=Ej07v2.pedirNumeroJO("introduce lado B");
+        ladoC=Ej07v2.pedirNumeroJO("Introduce lado C");
         
-        System.out.println("lado a"+ ladoA);
+        System.out.println("lado a "+ ladoA+ "lado b: "+ladoB+"lado c: "+ladoC);
     }
-    
+        public static double pedirNumeroJO(String texto){
+         String mensaje=(texto.isBlank())
+                ? "Introduce datos por teclado"
+            :texto;
+               
+         double numero=Double.parseDouble(mensaje);
+         return numero;
+
 }
