5aebb91ac0dd9b7ab4476d7a277f796a2c30e623
dnavmoy119 <dnavmoy119@g.educaand.es>
2023-01-11 10:58:44 +0100

    ejercicio 11 listo, comprueba resultados 10 y 11

--- src/main/java/EjerciciosAlgoritmos/Ej11.java
+++ src/main/java/EjerciciosAlgoritmos/Ej11.java
@@ -4,6 +4,8 @@
  */
 package EjerciciosAlgoritmos;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author daniel
@@ -11,7 +13,18 @@ package EjerciciosAlgoritmos;
 public class Ej11 {
     public static void main(String[] args) {
         
-        
+        double ladoH,ladoR,areaTotal;
+        ladoH=Ej10.pedirNumeroJO("Introduce lado H");
+        ladoR=Ej10.pedirNumeroJO("Introduce lado R");
+        areaTotal=calcularAreaRara(ladoH, ladoR);
+        JOptionPane.showMessageDialog(null, areaTotal);
     }
-    
+    public static double calcularAreaRara( double ladoH,double ladoR){
+        double area1,area2,areaTotal;
+        area1=(ladoH*(ladoR*2))/2;
+        area2=((ladoR*ladoR)*3.16)/2;
+        areaTotal=area1+area2;
+        
+        return areaTotal;
+    }
 }
